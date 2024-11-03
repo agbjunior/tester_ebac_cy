d3222482c57d3c8decaaf64f9d1a1a745239ccea
agbjunior <adilsonguimaraes1@hotmail.com>
2024-11-03 13:55:42 +0000

    massa de dados em lista

--- cypress/e2e/3- teste-ebac/produto.cy.js
+++ cypress/e2e/3- teste-ebac/produto.cy.js
@@ -5,22 +5,42 @@ import produtosPage from "../../support/page-objects/produtos.page";
 describe('Funcionalidade: Produto', () => {
     
     beforeEach(() => {
-        produtosPage.visitarUrl()
+        produtosPage.visitarUrl('product')
     });
     
     it('Deve selecionar um produto da lista', () => {
         produtosPage.buscarProdutoLista('Ajax Full-Zip Sweatshirt')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         produtosPage.buscarProduto('Ingrid Running Jacket')
         cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
         cy.get('.product_title').should('contain', 'Ingrid Running Jacket')
     });
     it('Deve visitar a pagina do produto especifico ', () => {
-        produtosPage.visitarProduto()
+        produtosPage.visitarProduto('ingrid running jacket')
+        cy.get('.product_title').should('contain', 'Ingrid Running Jacket')
+
     });
     it('Deve add o produto ao carrinho', () => {
+        produtosPage.buscarProduto('Ingrid Running Jacket')
+        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
         produtosPage.addProdutoLista()
+    });
+
+    it.only('Deve add o produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+            produtosPage.verCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade )
+            cy.get('.product-name > a').should('contain', dados[0].nomeProduto) 
+        });
+    })
+
+    it('Deve ir para o carrinho', () => {
+        produtosPage.buscarProduto('Ajax Full-Zip Sweatshirt')
+        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+        produtosPage.verCarrinho('L', 'Blue', 3)
+
     });
 })
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -15,12 +15,26 @@ class ProdutosPage {
         .click()
     }
 
-    visitarProduto(){
-
-
+    visitarProduto(nomeProduto){
+        //cy.visit(`produtos/${nomeProduto}`)
+        const urlFormatada = nomeProduto.replace(/ /g, '-')
+        cy.visit(`produtos/${urlFormatada}`)
     }
 
     addProdutoLista() {
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-White').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+    }
+
+    verCarrinho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click()
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
+        cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .buttons > .view-cart').click()
 
     }
 }
